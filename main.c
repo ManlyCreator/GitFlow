2e252d3ccd6a1df35f8d5e267e1337e632bbc3c5
Rami <myersrami17@gmail.com>
2024-10-12 08:36:51 -0500

    Takes the user's name from CLI arguments

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello, world\n");
+int main(int argc, char** argv) {
+  if (!argv[1]) {
+    printf("Usage: ./main <name>\n");
+    return 1;
+  }
+  printf("Hello, %s\n", argv[1]);
   return 0;
 }
